854427de5de1ed67e27c7733c6825c3c17914544
yan <yan@pop-os.localdomain>
2022-10-03 23:04:14 +0200

    work

--- sonos-cli/src/main.rs
+++ sonos-cli/src/main.rs
@@ -1,9 +1,6 @@
-use async_std::net::UdpSocket;
+use futures::{pin_mut, stream::StreamExt};
 use clap::{Parser, Subcommand};
-use futures::{
-    pin_mut,
-    stream::{Stream, StreamExt},
-};
+
 
 #[derive(Debug, Parser)]
 #[clap(name = "sonos")]
@@ -24,9 +21,7 @@ fn main() -> async_std::io::Result<()> {
     match cli.command {
         Command::Discover => {
             return async_std::task::block_on(async {
-                let stream = SSDP::broadcast(std::time::Duration::from_secs(2))
-                    .await?
-                    .listen();
+                let stream = ssdp::discover(ssdp::Builder::default()).await?;
                 pin_mut!(stream);
 
                 while let Some(device) = stream.next().await {
@@ -39,13 +34,31 @@ fn main() -> async_std::io::Result<()> {
     }
 }
 
-struct SSDP {
-    socket: UdpSocket,
-    timeout: std::time::Duration,
-}
+mod ssdp {
+    use futures::stream::Stream;
 
-impl SSDP {
-    pub async fn broadcast(timeout: std::time::Duration) -> std::io::Result<Self> {
+    pub struct Builder {
+        timeout: std::time::Duration,
+    }
+
+    impl Default for Builder {
+        fn default() -> Self {
+            Self { timeout: std::time::Duration::from_secs(1) }
+        }
+    }
+
+    impl Builder {
+        pub fn with_timeout(mut self, timeout: std::time::Duration) -> Self {
+            self.timeout = timeout;
+            self
+        }
+
+        pub async fn build(self) -> std::io::Result<impl Stream<Item = std::io::Result<String>>> {
+            discover(self).await
+        }
+    }
+
+    pub async fn discover(options: Builder) -> std::io::Result<impl Stream<Item = std::io::Result<String>>> {
         let socket = async_std::net::UdpSocket::bind("0.0.0.0:0").await?;
 
         let broadcast_addr = "239.255.255.250:1900";
@@ -59,43 +72,21 @@ impl SSDP {
             broadcast_addr, mx, search_target
         );
 
-        async_std::io::timeout(timeout, socket.send_to(msg.as_bytes(), broadcast_addr)).await?;
+        async_std::io::timeout(options.timeout, socket.send_to(msg.as_bytes(), broadcast_addr)).await?;
 
-        Ok(Self { socket, timeout })
+        Ok(async_stream::stream! {
+            loop {
+                let mut buf = [0u8; 2048];
+
+                let read = match async_std::io::timeout(options.timeout, socket.recv(&mut buf)).await {
+                    Ok(r) => r,
+                    Err(err) if err.kind() == std::io::ErrorKind::TimedOut => break,
+                    Err(err) => { yield Err(err); break },
+                };
+
+                yield Ok(String::from(std::str::from_utf8(&buf[..read]).unwrap()));
+            }
+        })
     }
 
-    async fn next_device(&self) -> Option<std::io::Result<String>> {
-        let mut buf = [0u8; 2048];
-
-        let read = match async_std::io::timeout(self.timeout, self.socket.recv(&mut buf)).await {
-            Ok(r) => r,
-            Err(err) if err.kind() == std::io::ErrorKind::TimedOut => return None,
-            Err(err) => return Some(Err(err)),
-        };
-
-        Some(Ok(String::from(std::str::from_utf8(&buf[..read]).unwrap())))
-    }
-
-    pub fn listen(self) -> impl Stream<Item = std::io::Result<String>> {
-        async_stream::stream! {
-           let broadcast = self;
-           while let Some(device) = broadcast.next_device().await {
-              yield device;
-           }
-        }
-    }
-}
-
-struct Device {
-    name: [u8; 256],
-}
-
-impl Device {
-    pub fn from_discover(buf: &[u8]) -> Self {
-        Self { name: [0u8; 256] }
-    }
-
-    pub fn name(&self) -> &str {
-        unsafe { std::str::from_utf8_unchecked(&self.name) }
-    }
 }
